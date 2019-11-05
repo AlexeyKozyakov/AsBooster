26bd4bcf8e8c6f043a35ad32a015283361b450a0
Alexey Kozyakov <alex-kozyakov@yandex-team.ru>
2019-11-05 23:51:57 +0700

    AB-3: create base app structure

---
+++ app/src/main/java/ru/nsu/fit/asbooster/audios/AudiosFragment.kt
@@ -0,0 +1,19 @@
+package ru.nsu.fit.asbooster.audios
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.nsu.fit.asbooster.R
+
+class AudiosFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
