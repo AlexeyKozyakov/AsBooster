263a71fbf10ae6162840fe8eb7f3a3992e05c8be
Alexey Kozyakov <alex-kozyakov@yandex-team.ru>
2019-12-08 02:40:02 +0700

    AB-18: smooth image loading

--- ASBooster/src/main/java/ru/nsu/fit/asbooster/audios/repository/ImageProvider.kt
+++ ASBooster/src/main/java/ru/nsu/fit/asbooster/audios/repository/ImageProvider.kt
@@ -1,21 +1,42 @@
 package ru.nsu.fit.asbooster.audios.repository
 
+import android.content.Context
 import android.widget.ImageView
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import ru.nsu.fit.asbooster.di.ActivityScoped
+import jp.wasabeef.picasso.transformations.BlurTransformation
+import java.lang.Exception
 import javax.inject.Inject
+import javax.inject.Singleton
 
 
-@ActivityScoped
-class ImageProvider @Inject constructor(private val picasso: Picasso) {
+@Singleton
+class ImageProvider @Inject constructor(
+    private val picasso: Picasso,
+    context: Context
+) {
 
-    fun provideImage(url: String?) = object : RequestedImage {
+    private val blurTransform = BlurTransformation(context, 1, 1)
+
+    //TODO: add error placeholder image
+    fun provideImage(imageUrl: String?, lowQualityImageUrl: String? = null) = object : RequestedImage {
         override fun show(view: ImageView) {
-            url?.let {
-                picasso.load(it).into(view)
-            }
+            lowQualityImageUrl?.let { lowQuality ->
+                picasso.load(lowQuality).transform(blurTransform).into(view, object : Callback {
+                    override fun onSuccess() {
+                        load(imageUrl, view)
+                    }
+
+                    override fun onError(e: Exception?) = Unit
+                })
+            } ?: load(imageUrl, view)
         }
     }
+
+    private fun load(imageUrl: String?, view: ImageView) = imageUrl?.let {
+        picasso.load(it).placeholder(view.drawable).into(view)
+    }
+
 }
 
 interface RequestedImage {
