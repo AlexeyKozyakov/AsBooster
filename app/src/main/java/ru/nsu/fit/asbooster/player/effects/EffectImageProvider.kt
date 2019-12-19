870d2b6218760f521526058445fcd3f5354e2f92
Alexey Kozyakov <alex-kozyakov@yandex-team.ru>
2019-12-19 21:25:03 +0700

    add reverb effect icon

--- app/src/main/java/ru/nsu/fit/asbooster/player/effects/EffectImageProvider.kt
+++ app/src/main/java/ru/nsu/fit/asbooster/player/effects/EffectImageProvider.kt
@@ -4,6 +4,7 @@ import ru.nsu.fit.asbooster.R
 import ru.nsu.fit.asbooster.di.ActivityScoped
 import ru.nsu.fit.asbooster.player.effects.preloaded.BassBoostEffect
 import ru.nsu.fit.asbooster.player.effects.preloaded.Effect
+import ru.nsu.fit.asbooster.player.effects.preloaded.ReverbEffect
 import ru.nsu.fit.asbooster.repository.ResourcesImageProvider
 import java.lang.IllegalArgumentException
 import javax.inject.Inject
@@ -15,6 +16,7 @@ class EffectImageProvider @Inject constructor(
 
     fun provideEffectImage(effect: Effect) = when(effect.id) {
         BassBoostEffect.ID -> imageProvider.privideImage(R.drawable.icon_bass_boost)
+        ReverbEffect.ID -> imageProvider.privideImage(R.drawable.reverb_icon)
         else -> throw IllegalArgumentException("Unsupported effect")
     }
 
