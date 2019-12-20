67a273339f2114fe3ca7f51f3c419918d39cbe60
Alexey Kozyakov <alex-kozyakov@yandex-team.ru>
2019-12-20 21:04:38 +0700

    AB-60: add new audios activity

--- app/src/main/java/ru/nsu/fit/asbooster/saved/SavedFragment.kt
+++ app/src/main/java/ru/nsu/fit/asbooster/saved/SavedFragment.kt
@@ -15,7 +15,6 @@ class SavedFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saved, container, false)
     }
 
