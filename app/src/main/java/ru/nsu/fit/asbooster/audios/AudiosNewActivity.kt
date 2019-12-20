b5175234b2181099cdd28dd87b8de5d89147ae71
Alexey Kozyakov <alex-kozyakov@yandex-team.ru>
2019-12-20 21:32:38 +0700

    AB-60: add fragments titles

--- app/src/main/java/ru/nsu/fit/asbooster/audios/AudiosNewActivity.kt
+++ app/src/main/java/ru/nsu/fit/asbooster/audios/AudiosNewActivity.kt
@@ -2,12 +2,17 @@ package ru.nsu.fit.asbooster.audios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import ru.nsu.fit.asbooster.R
+import ru.nsu.fit.asbooster.audios.ui.AudiosPagerAdapter
 
 class AudiosNewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audios_new)
+        findViewById<ViewPager>(R.id.audios_view_pager).adapter =
+            AudiosPagerAdapter(this, supportFragmentManager)
     }
 }
