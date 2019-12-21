718f07e6f9d456a4780d5ded204b9f42a9664e96
Paul Kholyavko <mrakakuy@yandex.ru>
2019-12-21 10:14:04 +0700

    AB-96: onShow test

--- app/src/test/java/ru/nsu/fit/asbooster/saved/SavedPresenterTest.kt
+++ app/src/test/java/ru/nsu/fit/asbooster/saved/SavedPresenterTest.kt
@@ -1,17 +1,32 @@
 package ru.nsu.fit.asbooster.saved
 
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import org.junit.Test
+import ru.nsu.fit.asbooster.repository.entity.AudioInfo
+import ru.nsu.fit.asbooster.saved.model.Track
 import ru.nsu.fit.asbooster.saved.model.TracksRepository
+import ru.nsu.fit.asbooster.saved.model.entity.EffectInfo
 import ru.nsu.fit.asbooster.search.navigation.Router
 import ru.nsu.fit.asbooster.view.ViewItemsMapper
 
 class SavedPresenterTest {
     private val savedView: SavedView = mock()
-    private val tracksRepository: TracksRepository = mock()
+
+    private val audioInfo: AudioInfo = mock()
+    private val effectInfo: EffectInfo = mock()
+    private val track: Track = mock {
+        on { audioInfo } doReturn audioInfo
+        on { effectsInfo } doReturn listOf(effectInfo)
+    }
+
+    private val tracksRepository: TracksRepository = mock {
+        on { getTracks() } doReturn listOf(track)
+    }
+
     private val viewItemsMapper: ViewItemsMapper = mock()
     private val uiScope: CoroutineScope = CoroutineScope(Dispatchers.Unconfined)
     private val router: Router = mock()
@@ -30,4 +45,13 @@ class SavedPresenterTest {
 
         verify(savedView).showProgress()
     }
+
+    @Test
+    fun `on show test`() {
+        savedPresenter.onShow()
+
+        verify(tracksRepository).getTracks()
+    }
+
+
 }
