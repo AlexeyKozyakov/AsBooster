eba8adeab4b64827d453145e7c84dfc2a9f447d9
Paul Kholyavko <mrakakuy@yandex.ru>
2019-12-21 10:33:41 +0700

    AB-98: InMemoryTracksRepository test class

---
+++ app/src/test/java/ru/nsu/fit/asbooster/saved/model/InMemoryTracksRepositoryTest.kt
@@ -0,0 +1,25 @@
+package ru.nsu.fit.asbooster.saved.model
+
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert
+import org.junit.Test
+import ru.nsu.fit.asbooster.repository.entity.AudioInfo
+import ru.nsu.fit.asbooster.saved.model.entity.EffectInfo
+
+class InMemoryTracksRepositoryTest {
+    private val audioInfo: AudioInfo = mock()
+    private val effectInfo: EffectInfo = mock()
+    private val track: Track = mock {
+        on { audioInfo } doReturn audioInfo
+        on { effectsInfo } doReturn listOf(effectInfo)
+    }
+
+    private val repository = InMemoryTracksRepository()
+
+    @Test
+    fun `save and get track`() {
+        repository.saveTrack(track)
+        Assert.assertEquals(track, repository.getTracks().single())
+    }
+}
