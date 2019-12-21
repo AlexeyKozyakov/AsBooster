f14536fcf065faadba194c3b2d25d297f16a60cd
Paul Kholyavko <mrakakuy@yandex.ru>
2019-12-21 09:49:34 +0700

    AB-95: get item wrong argument test

--- app/src/test/java/ru/nsu/fit/asbooster/audios/AudiosPaperAdapterTest.kt
+++ app/src/test/java/ru/nsu/fit/asbooster/audios/AudiosPaperAdapterTest.kt
@@ -3,6 +3,8 @@ package ru.nsu.fit.asbooster.audios
 import android.content.Context
 import androidx.fragment.app.FragmentManager
 import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert
+import org.junit.Test
 
 class AudiosPaperAdapterTest {
     private val context: Context = mock()
@@ -12,4 +14,15 @@ class AudiosPaperAdapterTest {
         context,
         fragmentManager
     )
+
+    @Test
+    fun `get wrong item test`() {
+        try {
+            audiosPaperAdapter.getItem(-1)
+        } catch (e: IllegalStateException) {
+            return
+        }
+
+        Assert.assertTrue(false)
+    }
 }
